7b18dac3a93a3be3190cc0bc1149858128bd4fc0
kevinweber <k-iwi@web.de>
2016-10-13 19:14:43 -0700

    Update parts according to updated React Boilerplate

--- internals/webpack/webpack.test.babel.js
+++ internals/webpack/webpack.test.babel.js
@@ -5,29 +5,19 @@
 const path = require('path');
 const webpack = require('webpack');
 const modules = [
+  'internals',
   'app',
   'node_modules',
 ];
 
 module.exports = {
   devtool: 'inline-source-map',
-  isparta: {
-    babel: {
-      presets: ['es2015', 'react', 'stage-0'],
-    },
-  },
   module: {
     // Some libraries don't like being run through babel.
     // If they gripe, put them here.
     noParse: [
       /node_modules(\\|\/)sinon/,
       /node_modules(\\|\/)acorn/,
-    ],
-    preLoaders: [
-      { test: /\.js$/,
-        loader: 'isparta',
-        include: path.resolve('app/'),
-      },
     ],
     loaders: [
       { test: /\.json$/, loader: 'json-loader' },
