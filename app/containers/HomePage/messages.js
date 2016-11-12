0aed58b6fac6f552432a831a631936c85129aa21
kevinweber <k-iwi@web.de>
2016-11-12 02:24:46 -0800

    Remove not needed texts

--- app/containers/HomePage/messages.js
+++ app/containers/HomePage/messages.js
@@ -6,14 +6,6 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  startProjectHeader: {
-    id: 'boilerplate.containers.HomePage.start_project.header',
-    defaultMessage: 'Start your next react project in seconds',
-  },
-  startProjectMessage: {
-    id: 'boilerplate.containers.HomePage.start_project.message',
-    defaultMessage: 'A highly scalable, offline-first foundation with the best DX and a focus on performance and best practices',
-  },
   upcomingHeader: {
     id: 'boilerplate.containers.HomePage.upcoming.header',
     defaultMessage: 'Upcoming',
@@ -28,6 +20,6 @@ export default defineMessages({
   },
   noResultsUpcoming: {
     id: 'boilerplate.containers.HomePage.noResults.upcoming',
-    defaultMessage: 'Upcoming events to be announced. Keep coming back. Check out past events to get a feeling when the next events will take place.',
+    defaultMessage: 'Upcoming events to be announced. Keep coming back.',
   },
 });
